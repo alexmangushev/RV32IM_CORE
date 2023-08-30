b3a5a4ac7e4f827930c3a6f78a2f47fb16dc6046
alexmangushev <mangushev2001@yandex.ru>
2023-08-30 22:43:47 +0300

    first commit

---
+++ include/rv_pkg.sv
@@ -0,0 +1,15 @@
+/***********************************************************************************
+ * Copyright (C) 2023 National Research University of Electronic Technology (MIET),
+ * Institute of Microdevices and Control Systems.
+ * See LICENSE file for licensing details.
+ *
+ * This file is a part of miriscv core.
+ *
+ ***********************************************************************************/
+
+package  rv_pkg;
+
+  parameter     XLEN       = 32;    // Data width, either 32 or 64 bit
+  parameter     ILEN       = 32;    // Instruction width, 32 bit only
+
+endpackage :  rv_pkg
